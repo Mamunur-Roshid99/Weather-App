5b8c09be90d8e2771edb40184e882d191d6f076e
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-12 20:47:29 +0600

    weatherapp

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    backgroundImage: {
+      "mobile-pattern": "url('/images/mobile.jpeg')",
+      "laptop-texture": "url('/images/laptop.jpeg')",
+    },
     screens: {
-        sm: "480px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
   },
   plugins: [],
